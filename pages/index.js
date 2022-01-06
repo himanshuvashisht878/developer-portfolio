6190da85195d14222c474eb97bb4a83e41b41a4a
Himanshu Vashisht <hv9238@bennett.edu.in>
2022-01-06 19:10:19 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,33 +19,19 @@ export default function Home({ githubProfileData }) {
 		<div>
 			<SEO
 				data={{
-					title: "Hanzla Tauqeer",
+					title: "Himanshu Vashisht",
 					description:
-						"A passionate Full Stack Web Developer and Mobile App Developer.",
-					image: "https://avatars3.githubusercontent.com/u/59178380?v=4",
-					url: "https://developer-portfolio-1hanzla100.vercel.app",
+						"A passionate Data Analyst.",
+					image: "https://avatars.githubusercontent.com/u/65850648?v=4",
+					url: "https://itshimanshu.netlify.app/",
 					keywords: [
-						"Hanzla",
-						"Hanzla Tauqeer",
-						"@1hanzla100",
-						"1hanzla100",
+						"Himanshu Vashisht",
+						"Himanshu",
+						"Data Analyst",
 						"Portfolio",
-						"Hanzla Portfolio ",
-						"Hanzla Tauqeer Portfolio",
-						"web developer",
-						"full stack",
-						"full stack web developer",
-						"mobile app developer",
-						"android developer",
-						"django",
-						"flask",
-						"django rest framework",
-						"nodejs ",
-						"expressjs",
-						"reactjs ",
-						"contextapi",
-						"redux",
-						"flutter",
+						"Himanshu Portfolio ",
+						"Himanshu Vashisht Portfolio",
+						"python",
 					],
 				}}
 			/>
